1542b81d34a7846aa8e8496fc0849d0af0acd421
Emil Renner Berthing <esmil@labitat.dk>
2019-03-25 20:59:02 +0100

    jumbotron: fakta: make a better bell sound

--- roles/jumbotron/files/fakta-bell.sh
+++ roles/jumbotron/files/fakta-bell.sh
@@ -2,30 +2,9 @@
 
 for i in 1 2 3; do
   gpio write 2 1
-  sleep 0.01
-  gpio write 2 0
   sleep 0.1
-  gpio write 2 1
-  sleep 0.01
   gpio write 2 0
-  sleep 0.01
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 0
-  sleep 0.01
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 0
-  sleep 0.1
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 0
-  sleep 0.01
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 1
-  sleep 0.01
-  gpio write 2 0
+  sleep 0.2
 done
+
+# vim: set ts=2 sw=2 et:
