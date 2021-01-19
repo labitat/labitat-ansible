d43cdbc412d6548447d3d4c6238fc56c99e09d98
Emil Renner Berthing <esmil@labitat.dk>
2021-01-19 21:58:10 +0100

    space_server: radius: use letsencrypt certificate

---
+++ roles/space_server/files/radius/certbot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+case "$RENEWED_LINEAGE" in
+*/space.labitat.dk)
+  install -m640 -o root -g radiusd \
+    "$RENEWED_LINEAGE/privkey.pem" \
+    "$RENEWED_LINEAGE/fullchain.pem" \
+    /etc/raddb/certs/
+  systemctl reload radiusd.service
+  ;;
+esac
+
+# vim: set ts=2 sw=2 et:
